1b3f4a47fd12ea0eb62f414ff643f1d00e20240c
shubhngirut2720 <shubhngirut27@gmail.com>
2021-01-18 15:22:31 +0530

    Added Library Management system Completed in frappe.

---
+++ library_management/library_management/doctype/artical/test_artical.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, shubhangi raut and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestArtical(unittest.TestCase):
+	pass
---
+++ library_management/library_management/doctype/library_transaction/library_transaction.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, shubhangi raut and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Library Transaction', {
+	// refresh: function(frm) {
+
+	// }
+});
